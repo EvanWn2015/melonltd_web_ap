c2a95385dd4e645864b15684fadef4177876b880
Evan Wang <jnswcy@gmail.com>
2018-06-14 22:21:05 +0800

    no message

--- melonltd-ap/src/main/java/com/melonltd/naber/rdbms/model/push/service/MailSendService.java
+++ melonltd-ap/src/main/java/com/melonltd/naber/rdbms/model/push/service/MailSendService.java
@@ -1,5 +1,6 @@
 package com.melonltd.naber.rdbms.model.push.service;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
@@ -48,36 +49,48 @@ public class MailSendService {
 				props.put("mail.smtp.starttls.enable", "true");
 			}
 
-			SESSION = Session.getInstance(props, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(USERNAME, PASSWORD);
-				}
-			});
+			SESSION = Session.getDefaultInstance(props, null);
+			// SESSION = Session.getInstance(props, new Authenticator() {
+			// @Override
+			// protected PasswordAuthentication getPasswordAuthentication() {
+			// return new PasswordAuthentication(USERNAME, PASSWORD);
+			// }
+			// });
 		}
 		return SESSION;
 	}
 
-	public void sendEmail(String toEmail, String subject, String body) throws MessagingException, UnsupportedEncodingException {
-//		try {
-//			
-			MimeMessage msg = new MimeMessage(getSession());
-			// set message headers
-			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-			msg.addHeader("format", "flowed");
-			msg.addHeader("Content-Transfer-Encoding", "8bit");
-			msg.setFrom(new InternetAddress(USERNAME, "NABER"));
-			msg.setReplyTo(InternetAddress.parse(USERNAME, false));
-			msg.setSubject(subject, "UTF-8");
-			msg.setText(body, "UTF-8");
-//			msg.setSentDate(new Date(Tools.getGMTDate("yyyy-MM-dd'T'HH:mm:ss.SSSS'Z'")));
-			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-			System.out.println("Message is ready");
-			Transport.send(msg);
-			System.out.println("EMail Sent Successfully!!");
-//		} catch (Exception e) {
-//			LOGGERO.error("send mail fail to mail : {}", toEmail);
-//		}
+	public void sendEmail(String toEmail, String subject, String body)
+			throws MessagingException, UnsupportedEncodingException {
+		// try {
+		//
+		MimeMessage msg = new MimeMessage(getSession());
+		// set message headers
+		msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+		msg.addHeader("format", "flowed");
+		msg.addHeader("Content-Transfer-Encoding", "8bit");
+		msg.setFrom(new InternetAddress(USERNAME, "NABER"));
+		msg.setReplyTo(InternetAddress.parse(USERNAME, false));
+		msg.setSubject(subject, "UTF-8");
+		msg.setText(body, "UTF-8");
+		// msg.setSentDate(new Date(Tools.getGMTDate("yyyy-MM-dd'T'HH:mm:ss.SSSS'Z'")));
+		msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+		System.out.println("Message is ready");
+		Transport.send(msg);
+		System.out.println("EMail Sent Successfully!!");
+		// } catch (Exception e) {
+		// LOGGERO.error("send mail fail to mail : {}", toEmail);
+		// }
 	}
+
+//	public static Message sendMessage(Gmail service, String userId, MimeMessage emailContent)
+//			throws MessagingException, IOException {
+//		Message message = createMessageWithEmail(emailContent);
+//		message = service.users().messages().send(userId, message).execute();
+//
+//		System.out.println("Message id: " + message.getId());
+//		System.out.println(message.toPrettyString());
+//		return message;
+//	}
 
 }
